31c5ad68f6388163d6a6f706257d9ad7b090bee0
Copperbotte <copperbotte@gmail.com>
2019-06-10 23:45:46 -0700

    Fixed optomization bug and generalized directory

--- Dynamic Loading.py
+++ Dynamic Loading.py
@@ -1,13 +1,13 @@
 import os
-
-funcs = dict()
+directory = "Modules"
 
 #filter python files from a folder
-mods = [x for x in os.listdir() if x[-3:] == '.py']
+mods = [x[:-3] for x in os.listdir(directory) if x[-3:] == '.py']
+funcs = dict()
 
 #import filtered files
 #filtered files self-register with the __register_plugin__ function
-modules = __import__("Modules", globals(), locals(), mods, 0)
+modules = __import__(directory, globals(), locals(), mods, 0)
 for m in mods:
     name, func = getattr(modules, m).register()
     funcs[name] = func
